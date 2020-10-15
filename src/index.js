06fae2bcaf1ff9132e97b61a54b8c17c75ab53d5
Fredrik Sundberg <fredrik.sundberg@skymill.io>
2020-10-15 13:32:31 +0200

    Move userData to separate JSON file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import userData from './data/users.json'
 
-const userData = JSON.parse(document.getElementById('user-data').dataset.users);
 ReactDOM.render(<App rows={userData} />, document.getElementById('root'));
